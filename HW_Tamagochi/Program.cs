cefdf5114bf1b52ad1d71a069e280958336c243e
KonstantinSF <kosmos008@gmail.com>
2023-03-22 14:58:03 +0300

    Add project files.

---
+++ HW_Tamagochi/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using HW_Tamagochi; 
+namespace System.Windows.Forms
+{
+
+
+    namespace HW_Tamagochi
+    {
+        internal class Program
+        {
+            
+
+                static void Main(string[] args)
+                {
+                    TamagochiPicture.ShowPic(); 
+                }
+
+
+                //for (int i = 0; i <256; i++)
+                //{
+                //   Write ($"{i}={(char)i}  ");
+                //}
+        }
+    }
+
+}
---
+++ HW_Tamagochi/TamagochiPicture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console; 
+
+namespace HW_Tamagochi
+{
+    public class TamagochiPicture
+    {
+        public static void ShowPic()
+        {
+            Console.SetWindowSize(54, 17);
+            bool eyeMove = true;
+            while (true)
+            {
+
+                if (eyeMove)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\n\t|_|_|_|_");
+                    WriteLine("\t/       \\");
+                    Write("\t|  ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Write((char)164 + "  " + (char)164);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine(" |"); eyeMove = false;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\t|    |  |");
+                    WriteLine("\t|   ___ |");
+                    WriteLine("\t|       |");
+                    WriteLine("\t|_______|");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\n\t|_|_|_|_");
+                    WriteLine("\t/       \\");
+                    Write("\t|   ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Write((char)164 + "  " + (char)164);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("|"); eyeMove = true;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    WriteLine("\t|    |  |");
+                    WriteLine("\t|   ___ |");
+                    WriteLine("\t|   \\_/ |");
+                    WriteLine("\t|_______|");
+                }
+
+                System.Threading.Thread.Sleep(800);
+                Console.Clear();
+            }
+        }
+    }
+}
