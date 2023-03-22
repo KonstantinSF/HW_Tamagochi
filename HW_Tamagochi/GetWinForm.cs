e3f748f0e535b5188d1aa2c9f61a55aee25dfa18
KonstantinSF <kosmos008@gmail.com>
2023-03-22 22:48:52 +0300

    не подключается messageBox

---
+++ HW_Tamagochi/GetWinForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace HW_Tamagochi
+{
+    internal class GetWinForm
+    {
+            public static System.Windows.Forms.DialogResult Show(string text);
+        private void DisplayMessageBoxText()
+        {
+            MessageBox.Show("Hello, world.");
+        }
+        private void button1_Click()
+        {
+            System.Forms.MessageBox.Show(
+                "Выберите один из вариантов",
+                "Сообщение",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+        }
+        //Show(IWin32Window, String, String, MessageBoxButtons, MessageBoxIcon)
+    }
+}
+
+
